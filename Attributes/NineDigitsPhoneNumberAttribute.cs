0dde6bcb2c2a08e996ff7fb827ed4b3269129039
Eduardo Félix <eduardo.felix@outlook.com>
2024-05-01 17:14:22 +0100

    Criação da classe para validação do telefone

---
+++ Attributes/NineDigitsPhoneNumberAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManagement.Attributes
+{
+    public class NineDigitsPhoneNumberAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var phoneNumber = (string)value;
+
+            if (string.IsNullOrEmpty(phoneNumber))
+            {
+                return ValidationResult.Success;
+            }
+
+            phoneNumber = phoneNumber.Replace(" ", "").Replace("-", "").Replace(".", "");
+
+            if (!long.TryParse(phoneNumber, out _))
+            {
+                return new ValidationResult("Phone number can only contain digits.");
+            }
+
+            if (phoneNumber.Length != 9)
+            {
+                return new ValidationResult("Phone number must have exactly 9 digits.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
