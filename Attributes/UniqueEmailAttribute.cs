39dff790e445ed39a365a20cf8241a0ae99b09ac
Eduardo Félix <eduardo.felix@outlook.com>
2024-05-01 17:15:00 +0100

    Criação da classe para verificação do email se é único ou não.

---
+++ Attributes/UniqueEmailAttribute.cs
@@ -0,0 +1,24 @@
+﻿using ContactManagement.Data;
+using ContactManagement.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactManagement.Attributes
+{
+    public class UniqueEmailAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var dbContext = (ApplicationDbContext)validationContext.GetService(typeof(ApplicationDbContext));
+            var email = (string)value;
+
+            var currentContactId = (validationContext.ObjectInstance as ContactModel)?.Id;
+
+            if (dbContext.contactModel.Any(c => c.Email == email && c.Id != currentContactId))
+            {
+                return new ValidationResult("This email is already in use.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
