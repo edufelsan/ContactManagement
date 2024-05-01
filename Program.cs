c23914853b1711f837730f2d158011ba40993935
Eduardo Félix <eduardo.felix@outlook.com>
2024-05-01 17:11:00 +0100

    Adicionada configurações

--- Program.cs
+++ Program.cs
@@ -1,9 +1,39 @@
+using ContactManagement.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Runtime.InteropServices;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string configFileName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "appsettings.Development.json" : "appsettings.json";
+builder.Configuration.AddJsonFile(configFileName, optional: false, reloadOnChange: true);
+
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 26));
+
+var connectionString = builder.Configuration.GetConnectionString("MariaDB");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, serverVersion)
+);
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetService<ApplicationDbContext>();
+
+    if (dbContext != null && dbContext.Database.GetDbConnection().State != ConnectionState.Open)
+    {
+        dbContext.Database.OpenConnection();
+
+        // Executar EnsureCreated() para criar o banco de dados e quaisquer migrações pendentes
+        dbContext.Database.EnsureCreated();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -18,10 +48,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Contact}/{action=Index}/{id?}");
 
 app.Run();
