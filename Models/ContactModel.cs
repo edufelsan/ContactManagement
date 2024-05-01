28380fde77b987ea03814a2c0c9f1071f0a13cc1
Eduardo Félix <eduardo.felix@outlook.com>
2024-05-01 17:12:58 +0100

    Criação do modelo de dados ContactModel

---
+++ Models/ContactModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using ContactManagement.Attributes;
+
+namespace ContactManagement.Models
+{
+    [Table("Customers")]
+    public class ContactModel
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        [Required]
+        public string? Name { get; set; }
+
+        [Column("contact")]
+        [Required]
+        [NineDigitsPhoneNumber]
+        public string? Contact { get; set; }
+
+        [Column("email")]
+        [Required]
+        [EmailAddress(ErrorMessage = "The Email field is not in a valid format.")]
+        [UniqueEmail(ErrorMessage = "Este e-mail já está em uso.")]
+        public string? Email { get; set; }
+    }
+}
