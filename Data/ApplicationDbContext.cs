de2ddd9dcbfed0f8fce94cff5276d171e663ab10
Eduardo Félix <eduardo.felix@outlook.com>
2024-05-01 17:13:39 +0100

    Criação e configuração do DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ContactManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManagement.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContactModel> contactModel { get; set; }
+    }
+}
